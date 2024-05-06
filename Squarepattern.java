a8ad8b9b2210eb53e06c0a7d867a9321d27ef6c4
Shivani <96380541+afcatshivani@users.noreply.github.com>
2024-05-06 21:52:28 +0530

    Add files via upload

---
+++ Squarepattern.java
@@ -0,0 +1,17 @@
+//Square pattern
+
+public class Squarepattern {
+
+    public static void main(String[] args) {
+        for(int i=1; i<=4; i++){
+            System.out.println("****");
+        }
+
+
+        // int i =1;
+        // while (i<=4) {
+        //     i++;
+        //     System.out.println("****");
+        // }
+    }
+}
